938dab248c4439b920560f5d25de025844e9f52a
David Velasquez <velasquezaguilerad@gmail.com>
2025-05-27 22:34:53 -0700

    Update Book component with end cover and image fixes

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -79,7 +79,7 @@ function Book() {
       <div className="page" style={{ background: 'transparent' }}>
         <div className="page-content cover">
           <img 
-            src="./assets/Top-Favorite-Pokemon-5-23-2025.jpg" 
+            src="/assets/Top-Favorite-Pokemon-5-23-2025.jpg" 
             alt="assets/Top-Favorite-Pokemon-5-23-2025" 
             className="pokemon-logo"
           />
@@ -118,7 +118,7 @@ function Book() {
           <h1 style={{ textAlign: 'center' }}>The End</h1>
           <p style={{ textAlign: 'center' }}>Thanks for reading my Top 10 Pokémon picks!</p>
           <img 
-            src=".\assets\wooper-removebg-preview.png" 
+            src="\assets\wooper-removebg-preview.png" 
             alt="wooper thank you" 
             style={{ width: '150px', marginTop: '20px' }}
           />
